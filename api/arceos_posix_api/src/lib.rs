7752fc6a721306610f7180ae936c15dec13b7f2c
Stone Simpson <dio_ro@outlook.com>
2025-01-09 17:59:54 +0800

    some modifications for openat & getdents64

--- api/arceos_posix_api/src/lib.rs
+++ api/arceos_posix_api/src/lib.rs
@@ -40,7 +40,7 @@ pub use imp::time::{sys_clock_gettime, sys_nanosleep, sys_get_time_of_day};
 #[cfg(feature = "fd")]
 pub use imp::fd_ops::{sys_close, sys_dup, sys_dup2, sys_fcntl, FD_TABLE};
 #[cfg(feature = "fs")]
-pub use imp::fs::{sys_fstat, sys_getcwd, sys_lseek, sys_lstat, sys_open, sys_rename, sys_stat};
+pub use imp::fs::{sys_fstat, sys_getcwd, sys_lseek, sys_lstat, sys_open, sys_rename, sys_stat, sys_openat, Directory};
 #[cfg(feature = "select")]
 pub use imp::io_mpx::sys_select;
 #[cfg(feature = "epoll")]
